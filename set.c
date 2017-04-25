4233fd7babeb78b60299281f440d5e3ea5cf02f1
Shen Zhu <shenzhu0127@gmail.com>
2017-04-24 20:16:33 -0400

    add and test get_set_type(), get_set_size(), check_set_element(), put_set()

---
+++ set.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include "set.h"
+
+
+struct Set *create_set(int32_t type) {
+    // Apply for memory
+    struct Set *newSet = (struct Set *) malloc(sizeof(struct Set));
+
+    // Set property
+    newSet->type = type;
+    newSet->size = 0;
+    newSet->data = create_list(type);
+
+    return newSet;
+}
+
+bool check_set_element(struct Set *set, ...) {
+    // Corner case
+    if (set == NULL) {
+        printf("%s\n", "Error! check_set_element : Set does not exist.\n");
+        exit(1);
+    }
+
+    bool exist = 0;
+
+    va_list args_ptr;
+    va_start(args_ptr, set);
+
+    switch (set->type) {
+        case INT:
+            exist = check_list_element(set->data, va_arg(args_ptr, int));
+            break;
+
+        case FLOAT:
+            exist = check_list_element(set->data, va_arg(args_ptr, double));
+            break;
+
+        case BOOL:
+            exist = check_list_element(set->data, va_arg(args_ptr, bool));
+            break;
+
+        case STRING:
+            exist = check_list_element(set->data, va_arg(args_ptr, char*));
+            break;
+
+        default:
+            break;
+    }
+
+    va_end(args_ptr);
+
+    return exist;
+}
+
+struct Set *put_set(struct Set *set, ...) {
+    // Corner case
+    if (set == NULL) {
+        printf("%s\n", "Error! put_set : Set does not exist.\n");
+        exit(1);
+    }
+
+    void *addData;
+
+    va_list args_ptr;
+    va_start(args_ptr, set);
+    switch (set->type) {
+        case INT:
+            addData = intTovoid(va_arg(args_ptr, int));
+            if (!check_set_element(set, voidToint(addData))) {
+                set->data = plus_list(set->data, voidToint(addData));
+                set->size++;
+            } else {
+                printf("Error! put_set : Element Already exist.\n");
+                exit(1);
+            }
+            break;
+
+        case FLOAT:
+            addData = floatTovoid(va_arg(args_ptr, double));
+            if (!check_set_element(set, voidTofloat(addData))) {
+                set->data = plus_list(set->data, voidTofloat(addData));
+                set->size++;
+            } else {
+                printf("Error! put_set : Element Already exist.\n");
+                exit(1);
+            }
+            break;
+
+        case BOOL:
+            addData = boolTovoid(va_arg(args_ptr, bool));
+            if (!check_set_element(set, voidTobool(addData))) {
+                set->data = plus_list(set->data, voidTobool(addData));
+                set->size++;
+            } else {
+                printf("Error! put_set : Element Already exist.\n");
+                exit(1);
+            }
+            break;
+
+        case STRING:
+            addData = stringTovoid(va_arg(args_ptr, char*));
+            if (!check_set_element(set, voidTostring(addData))) {
+                set->data = plus_list(set->data, voidTostring(addData));
+                set->size++;
+            } else {
+                printf("Error! put_set : Element Already exist.\n");
+                exit(1);
+            }
+            break;
+
+        default:
+            break;
+    }
+
+    va_end(args_ptr);
+
+    return set;
+}
+
+int32_t get_set_type(struct Set *set) {
+    if (set == NULL) {
+        printf("%s\n", "Error! get_set_type : Set does not exist.\n");
+        exit(1);
+    }
+
+    return set->type;
+}
+
+int32_t get_set_size(struct Set *set) {
+    if (set == NULL) {
+        printf("%s\n", "Error! get_set_type : Set does not exist.\n");
+        exit(1);
+    }
+
+    return set->size;
+}
+
+
+int main() {
+    // Test function: create_set, get_set_type
+    printf("%s\n", "TEST: create_set");
+    struct Set *intSet = create_set(INT);
+    printf("%d\n", intSet->type);
+    printf("%d\n", intSet->data->type);
+    printf("%d\n", intSet->size);
+    printf("%d\n", get_set_type(intSet));
+
+    struct Set *stringSet = create_set(STRING);
+    printf("%d\n", stringSet->type);
+    printf("%d\n", stringSet->data->type);
+    printf("%d\n", stringSet->size);
+    printf("%d\n", get_set_type(stringSet));
+
+
+    // Test function: put_set, get_set_size
+    printf("%s\n", "TEST: put_set, get_set_size");
+    struct Set* intSet2 = create_set(INT);
+    intSet2 = put_set(intSet2, 1);
+    printf("%d\n", get_set_size(intSet2));
+    assert(get_set_size(intSet2) == 1);
+    intSet2 = put_set(intSet2, 2);
+    printf("%d\n", get_set_size(intSet2));
+    assert(get_set_size(intSet2) == 2);
+    intSet2 = put_set(intSet2, 3);
+    printf("%d\n", get_set_size(intSet2));
+    assert(get_set_size(intSet2) == 3);
+
+    struct Set* stringSet2 = create_set(STRING);
+    stringSet2 = put_set(stringSet2, "hello");
+    stringSet2 = put_set(stringSet2, "world");
+    printf("%d\n", check_set_element(stringSet2, "hello"));
+    printf("%d\n", check_set_element(stringSet2, "world"));
+    printf("%d\n", check_set_element(stringSet2, "columbia"));
+}
--- set.h
+++ set.h
@@ -2,11 +2,29 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdarg.h>
+#include "list.c"
 
 
 #ifndef TUSIMPLELIB_SET_H
 #define TUSIMPLELIB_SET_H
 
+
+struct Set {
+    int32_t type;
+    int32_t size;
+    struct List *data;
+};
+
+
+struct Set *create_set(int type);
+
+bool check_set_element(struct Set *set, ...);
+
+struct Set *put_set(struct Set *set, ...);
+
+int32_t get_set_type(struct Set *set);
+
+int32_t get_set_size(struct Set *set);
 
 
 #endif //TUSIMPLELIB_SET_H
